f2357116922057d6953e57861d1506463a912422
Aeoly <eolenaeia@gmail.com>
2024-10-28 19:54:11 +0200

    configured main page, added favicon, gitignored *.pyc files

--- hromada/urls.py
+++ hromada/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from home.views import main_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('main/', main_view)
 ]

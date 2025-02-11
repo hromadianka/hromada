8e669845fd1dc667fc50cb075b81f80186d84db3
Karyna <karynachmeliuk@gmail.com>
2025-02-11 18:10:53 +0200

    add homepage survey processing

--- home/models.py
+++ home/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class SurveyResponse(models.Model):
+    age = models.PositiveIntegerField()
+    income_option = models.CharField(max_length=255)
+    social_status_option = models.CharField(max_length=255)
+    profession = models.CharField(max_length=255, blank=True, null=True)
+    field = models.CharField(max_length=255, blank=True, null=True)
+    education_option = models.CharField(max_length=255)
+    veterans_option = models.CharField(max_length=255, blank=True, null=True)
+    dead_veterans_option = models.CharField(max_length=255, blank=True, null=True)
+    birthplace = models.CharField(max_length=255)
+    residence = models.CharField(max_length=255)
+    participating_option = models.CharField(max_length=255)
+    participating_about_question = models.TextField(blank=True, null=True)
+    participating_reasons_question = models.TextField(blank=True, null=True)
+    participating_more_question = models.TextField(blank=True, null=True)
+    interest_level_1 = models.CharField(max_length=255)
+    interest_level_2 = models.CharField(max_length=255)
+    why_interest_rate_question = models.TextField(blank=True, null=True)
+    government_option = models.CharField(max_length=255)
+    government_additional_question = models.TextField(blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"SurveyResponse {self.id} - {self.age} years old"
--- home/views.py
+++ home/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+﻿from django.shortcuts import render
+from django.contrib import messages
+from .models import SurveyResponse
 
 # Create your views here.
 
 def home_page(request):
     return render(request, 'home_page.html')
+
+def survey(request):
+    if request.method == "POST":
+        fields = [
+            "age", "income-option", "social-status-option", "profession", "field",
+            "education-option", "veterans-option", "dead-veterans-option",
+            "birthplace", "residence", "participating-option",
+            "participating-about-question", "participating-reasons-question",
+            "participating-more-question", "interest-level-1", "interest-level-2",
+            "why-interest-rate-question", "government-option", "government-additional-question"
+        ]
+
+        data = {field.replace("-", "_"): request.POST.get(field, None) for field in fields}
+        SurveyResponse.objects.create(**data)
+        
+        messages.success(request, "Ваша відповідь успішно збережена. Ми дуже вдячні вам за участь❤")
+
+    return render(request, "home_page.html")

aa1ffa22912d95e4237efa61c0b16d5f6a41d3e4
hromadianka <99715792+hromadianka@users.noreply.github.com>
2025-02-25 22:44:17 +0200

    Update settings.py

--- hromada/settings.py
+++ hromada/settings.py
@@ -102,7 +102,8 @@ if jawsdb_url:
             'HOST': url.hostname,
             'PORT': url.port,
             'OPTIONS': {  
-                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+                'charset': 'utf8mb4'
             }  
         }
     } 

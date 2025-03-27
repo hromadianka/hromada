a829ea8a08605ceae617b3fe83847d326d47e5e9
hromadianka <99715792+hromadianka@users.noreply.github.com>
2025-03-27 14:26:32 +0200

    Fix search page bug

--- search/views.py
+++ search/views.py
@@ -5,4 +5,4 @@ from project.models import Project
 
 def search(request):
     all_projects = Project.objects.all()
-    return (request, 'search.html', {'all_projects': all_projects})
+    return render(request, 'search.html', {'all_projects': all_projects})

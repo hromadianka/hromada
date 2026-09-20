7e5f68ce24aa9a33c3df9e25ca6d9c9ab8c7e714
hromadianka <99715792+hromadianka@users.noreply.github.com>
2026-09-20 11:04:12 +0300

    Update settings.py

--- hromada/settings.py
+++ hromada/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # DEBUG = True
 
 ALLOWED_HOSTS = ["hromada-3fc3a43a6372.herokuapp.com", "hromada.me", "www.hromada.me"]
